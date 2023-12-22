72d83295185a46c73c836e208410734ea2428a9a
michelmnds <m1tchofc@gmail.com>
2023-12-22 08:37:28 +0000

    feat: basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { LandingPage } from "./pages/LandingPage";
+import { ProfilePage } from "./pages/ProfielPage";
+import { SingleCharacterPage } from "./pages/SingleCharacterPage";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <Routes>
+      <Route element={<HomePage />} path="/home" />
+      <Route element={<LandingPage />} path="/" />
+      <Route element={<ProfilePage />} path="/profile" />
+      <Route element={<SingleCharacterPage />} path="/character/:charId" />
+    </Routes>
+  );
 }
 
 export default App;
