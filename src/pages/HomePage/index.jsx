fb109fdbf5e010a2ebda072449fa6a73e3083d6c
michelmnds <m1tchofc@gmail.com>
2024-01-02 20:39:04 +0000

    feat: created header component

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,32 @@
 import { useContext, useEffect } from "react";
 import { CharactersContext } from "../../providers/CharacterContext";
 import { UserContext } from "../../providers/UserContext";
+import { Header } from "../../components/Header";
+import "./style.css";
 
 export const HomePage = () => {
+  // eslint-disable-next-line no-unused-vars
   const { triggerRefresh, renderCharacters, renderPages, fetchCharacters } =
     useContext(CharactersContext);
   const { currentHouse } = useContext(UserContext);
 
   useEffect(() => {
     fetchCharacters();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentHouse]);
 
   return (
     <>
-      <h1>
+      <Header />
+
+      <h1 className="title">
         All characters
         {currentHouse === null ? "" : ` of house ${currentHouse}`}
       </h1>
-      <button type="button" onClick={triggerRefresh}>
+      {/* <button type="button" onClick={triggerRefresh}>
         Refresh
-      </button>
-      <ul>{renderCharacters()}</ul>
+      </button> */}
+      <ul className="charList">{renderCharacters()}</ul>
 
       {renderPages()}
     </>
