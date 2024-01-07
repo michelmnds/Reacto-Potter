bce29a10acdb767801c1bd98a69bb4cb64c04b9b
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-07 21:45:18 +0100

    feat: added skelleton for house quiz questions and answers and context to store scores, answers now get randomized every page change

--- src/pages/HouseQuizPage/index.jsx
+++ src/pages/HouseQuizPage/index.jsx
@@ -1,14 +1,66 @@
-import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { QuizContext } from "../../providers/QuizContext";
+import { useContext, useEffect, useState } from "react";
 
 export const HouseQuizPage = () => {
   const { quizpage } = useParams();
+  const { quiz } = useContext(QuizContext);
+  const [answerOrder, setAnswerOrder] = useState([
+    "gryffindorAnswer",
+    "slytherinAnswer",
+    "ravenclawAnswer",
+    "huffelpuffAnswer",
+  ]);
   //ensures navigation to next quiz question and after 10 total questions sends user to results page
   const nextPage = quizpage < 10 ? parseInt(quizpage) + 1 : "results";
+
+  //randomizes order of answers
+  const randomizeAnswers = () => {
+    console.log("test");
+    const randomAnswerOrder = answerOrder;
+    for (let i = randomAnswerOrder.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomAnswerOrder[i], randomAnswerOrder[j]] = [
+        randomAnswerOrder[j],
+        randomAnswerOrder[i],
+      ];
+    }
+    setAnswerOrder(randomAnswerOrder);
+  };
+
+  //rerender randomized order of answers
+  useEffect(() => {
+    randomizeAnswers();
+  }, [quizpage]);
+
   return (
     <>
-      <h2>House Quiz Question {quizpage} </h2>
-      <Link to={`/quiz/${nextPage}`}>Hogwards answer -- go to next page </Link>
+      {/*Either create logic to render results in quiz context or create separate context - also classes needed to style different answers
+      --also: idea to randomize order of 4 answer options - make create randomizer function */}
+      {quizpage === "results" ? (
+        <p>Your House is ...</p>
+      ) : (
+        <div>
+          <h2>{quiz[quizpage - 1].question} </h2>
+          <ol>
+            <Link to={`/quiz/${nextPage}`}>
+              <li>{quiz[quizpage - 1][answerOrder[0]]}</li>
+            </Link>
+
+            <Link to={`/quiz/${nextPage}`}>
+              <li>{quiz[quizpage - 1][answerOrder[1]]}</li>
+            </Link>
+
+            <Link to={`/quiz/${nextPage}`}>
+              <li>{quiz[quizpage - 1][answerOrder[2]]}</li>
+            </Link>
+
+            <Link to={`/quiz/${nextPage}`}>
+              <li>{quiz[quizpage - 1][answerOrder[3]]}</li>
+            </Link>
+          </ol>
+        </div>
+      )}
     </>
   );
 };
--- src/providers/QuizContext.jsx
+++ src/providers/QuizContext.jsx
@@ -11,7 +11,79 @@ export const QuizContextProvider = ({ children }) => {
   };
   const [currentHouseScores, setCurrentHouseScores] =
     useState(resetHouseScores);
-  const [currentQuizPage, setCurrentQuizPage] = useState("");
+  const [currentQuizPage, setCurrentQuizPage] = useState(1);
+  const quiz = [
+    {
+      question: "question1text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question2text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question3text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question4text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question5text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question6text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question7text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question8text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question9text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+    {
+      question: "question10text?",
+      gryffindorAnswer: "gryffindorAnswerText",
+      slytherinAnswer: "slytherinAnswerText",
+      ravenclawAnswer: "ravenclawAnswerText",
+      huffelpuffAnswer: "huffelpuffAnswerText",
+    },
+  ];
 
   return (
     <QuizContext.Provider
@@ -20,6 +92,7 @@ export const QuizContextProvider = ({ children }) => {
         setCurrentHouseScores,
         currentQuizPage,
         setCurrentQuizPage,
+        quiz,
       }}
     >
       {children}
