50cdbef55bfdaf07dce17c915fb467b689c48168
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-09 15:01:40 +0100

    work on modal

--- src/components/ModalHouse/index.jsx
+++ src/components/ModalHouse/index.jsx
@@ -1,36 +1,34 @@
 import "./style.css";
 import ReactModal from "react-modal";
 import { useForm } from "react-hook-form";
+import { HouseContext } from "../../providers/HouseContext";
+import { useContext } from "react";
 
-export const ModalHouse = ({
-  isModalOpen,
-  handleCloseModal,
-  changeHouseName,
-  changeHouseImage,
-}) => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+export const ModalHouse = ({ isModalOpen, handleCloseModal }) => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => createHouse(data);
+  const { createHouse, houses } = useContext(HouseContext);
+  //event.target.key for getting id from the house card
+
   return (
     <>
       <ReactModal
         isOpen={isModalOpen}
+        shouldCloseOnOverlayClick={true}
+        onRequestClose={handleCloseModal}
         style={{
           content: { top: "30vh", left: "30vw", right: "30vw", bottom: "30vh" },
         }}
       >
         <h2 className="modalHeading">Create new house</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
-            defaultValue={!changeHouseName ? "" : changeHouseName}
+            defaultValue=""
             {...register("name")}
             placeholder="House Name"
           />
           <input
-            defaultValue={!changeHouseImage ? "" : changeHouseImage}
+            defaultValue=""
             {...register("image")}
             placeholder="House Image Link"
           />
